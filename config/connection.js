bebfa63200c5c675ca603a91fa72960715ef1676
Your Name <jasjohn3@uat.edu>
2021-08-31 18:34:45 -0700

    Initial server setup with sequelize and express router

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,12 @@
-const sequelize = require('sequelize');
+const Sequelize = require('sequelize');
+
 require('dotenv').config();
-const DB_NAME = process.env.DB_NAME;
-const DB_USER = process.env.DB_USER;
-const DB_PW = process.env.DB_PW;
-const sequelize = new sequelize(DB_NAME, DB_USER, DB_PW, {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306,
-})
+
+// create connection to our db
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+  host: 'localhost',
+  dialect: 'mysql',
+  port: 3306
+});
 
 module.exports = sequelize;

e8761e68c727966f9b364164af996324f982e33f
Your Name <jasjohn3@uat.edu>
2021-09-04 21:14:13 -0700

    Updated User.js

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
 // create our User model
 class User extends Model {}
@@ -32,6 +33,15 @@ User.init(
         len: [4]
       }
     }
+  },
+  {
+    hooks: {
+      // set up beforeCreate lifecycle "hook" functionality
+      async beforeCreate(newUserData) {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+      }
   },
   {
     sequelize,

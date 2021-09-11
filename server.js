92841e8859fbef0920d6d4fe9842e33fb12e60ac
Your Name <jasjohn3@uat.edu>
2021-09-11 09:37:25 -0700

    Setup routes but experiencing an issue with sequelize connecting to the db

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
-const app = require('express')
+const express = require('express')
+const routes = require('./routes');
+const sequelize = require('./config/connection');
+
+const app = express();
+const PORT = process.env.PORT || 3001
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+//turn on routes
+app.use(routes);
+
+// connect to the db and server
+
+sequelize.sync({force:false})
+.then(()=>{
+  app.listen(PORT,()=> console.log(`Now Listening on Port: ${PORT}`))
+})
